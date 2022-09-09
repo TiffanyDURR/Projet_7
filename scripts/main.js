7fcd501e3d43c6291524fda93a80166d9d9db1cb
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-09-09 16:14:09 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import {recipes} from "../data/recipes.js"
-import { displayFilters } from "./displayfilters.js";
+import {filters} from "./filters.js";
 import {JSstyles} from "./styles.js";
 
 const cardsContent = document.querySelector(".cards-content");
