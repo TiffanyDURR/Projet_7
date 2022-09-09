e6de575fea3cbfdddc3a5dedf347ee6146587e46
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-09-09 16:14:28 +0200

    Update styles.js

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,4 +1,4 @@
-export function JSstyles () {
+export function styles () {
 
 let removeIngredientTag = document.querySelector(".ingClose");
 let removeAppareilTag = document.querySelector(".appClose");
