b474d76ae838c99cd1176e528caa7616aca91d77
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-09-12 13:42:53 +0200

    Update filters.js

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,8 +1,10 @@
 import {recipes} from "../data/recipes.js"
 import {displayCards} from "./main.js";
+let data = [];
+data = recipes;
 
-const appareilsList = document.querySelector(".appareils-list");
-const inputAppareils = document.getElementById("inputAppareils");
+const appareilsContent = document.querySelector(".appareils-list");
+const searchAppareils = document.getElementById("inputAppareils");
 const ingredientsList = document.querySelector(".ingredients-list");
 const inputIngredients = document.getElementById("inputIngredients");
 const ustensilesList = document.querySelector(".ustensiles-list");
@@ -10,7 +12,48 @@ const inputUstensiles = document.getElementById("inputUstensiles");
 const ingredientTagAdd = document.querySelector(".ingredients-selected > span");
 
 export function filters () {
+
+    function initFilters () {
+displayAppareils(recipes); 
+appareilsSearch();
+    }
+
+    initFilters();
+
+    function displayAppareils(recipes) {
+        const appareilsList = 
+        recipes.map ((recipe) => {
+            let appareilsData = []; 
+            appareilsData = [... new Set(appareilsData)]
+            console.log(appareilsData)
+            let appareilsWay = recipe.appliance;
+            appareilsData.push(`<span data-appareil="${recipe.appliance}">${appareilsWay}</span>`)
+            return appareilsData
+        }
+        ).join("");
+        appareilsContent.innerHTML = appareilsList;
+    }
+
+    function appareilsSearch () {
+        searchAppareils.addEventListener("input", (e) => {
+            const inputSearchAppareils = e.target.value.toLowerCase();
+            const filteredAppareilsTag = data
+            .filter((recipe) => {
+                return (
+                    recipe.appliance.toLocaleLowerCase().includes(inputSearchAppareils)
+                );
+            });
+            displayAppareils(filteredAppareilsTag);
+            // displayCards(filteredAppareilsTag);
+        })
+        }
     
+        
+    
+    
+
+
+
 } // Fin export filters
 
 
