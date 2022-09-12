251cf41b5fa7a33a564fc9858c730883a1d04ac2
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-09-12 11:28:37 +0200

    Update filters.js

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,5 +1,16 @@
+import {recipes} from "../data/recipes.js"
+import {displayCards} from "./main.js";
 
-function filters () {
-}
+const appareilsList = document.querySelector(".appareils-list");
+const inputAppareils = document.getElementById("inputAppareils");
+const ingredientsList = document.querySelector(".ingredients-list");
+const inputIngredients = document.getElementById("inputIngredients");
+const ustensilesList = document.querySelector(".ustensiles-list");
+const inputUstensiles = document.getElementById("inputUstensiles");
+const ingredientTagAdd = document.querySelector(".ingredients-selected > span");
 
-filters();
+export function filters () {
+    
+} // Fin export filters
+
+
